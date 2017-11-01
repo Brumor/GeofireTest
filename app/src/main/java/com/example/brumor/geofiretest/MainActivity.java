6223c4b823aa74f2e6ef08086825f592c49e3fcb
Brumor <paulbricout2@gmail.com>
2017-11-02 00:54:49 +0100

    installed Geofire

--- app/src/main/java/com/example/brumor/geofiretest/MainActivity.java
+++ app/src/main/java/com/example/brumor/geofiretest/MainActivity.java
@@ -3,11 +3,18 @@ package com.example.brumor.geofiretest;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.firebase.geofire.GeoFire;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference("path/to/geofire");
+        GeoFire geoFire = new GeoFire(databaseReference);
     }
 }
