a64b176b8db5f72550817d6b3642e2dc1186defc
Brumor <paulbricout2@gmail.com>
2017-11-03 20:14:51 +0100

    implement FirebaseCrash + Bug to correct!!

--- app/src/main/java/com/example/brumor/geofiretest/MainActivity.java
+++ app/src/main/java/com/example/brumor/geofiretest/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.brumor.geofiretest;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -10,11 +12,25 @@ import android.widget.Toast;
 import com.firebase.geofire.GeoFire;
 import com.firebase.geofire.GeoLocation;
 import com.firebase.geofire.GeoQuery;
+import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
+
+    public final static String TAG = "Main Activity";
+
+    public final int[] id = {0};
 
     Button sendButton;
 
@@ -26,13 +42,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        id[0] = 0;
 
         sendButton = findViewById(R.id.send_message);
 
         messageEditText = findViewById(R.id.message_text);
 
-        final DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference("test/geomessage/geofire");
-        final GeoFire geoFire = new GeoFire(databaseReference);
+        final DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference("test/geomessage/");
 
         sendButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -40,31 +56,87 @@ public class MainActivity extends AppCompatActivity {
 
                 message = messageEditText.getText().toString();
 
-                GeoMessage currentGeomessage = new GeoMessage(message);
+                Log.e(TAG, "Test 1");
+                GeoMessage currentGeomessage = new GeoMessage(id[0], message);
 
-                databaseReference.push().setValue(currentGeomessage);
-                geoFire.setLocation("test", new GeoLocation(50, 50), new GeoFire.CompletionListener() {
+                Log.e(TAG, "Test 2");
+
+                databaseReference.child("children").push().setValue(currentGeomessage).addOnSuccessListener(new OnSuccessListener<Void>() {
                     @Override
-                    public void onComplete(String key, DatabaseError error) {
-
-                        Toast.makeText(getApplicationContext(), "Send.", Toast.LENGTH_LONG).show();
-
+                    public void onSuccess(Void aVoid) {
+                        Log.e(TAG, "Success !");
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.e(TAG, "FAIL");
+                    }
+                }).addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        Log.e(TAG, "Complete");
                     }
                 });
+
+                Log.e(TAG, "Test 3");
+            }
+        });
+
+
+        databaseReference.child("children").addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                Log.e("101", "Child Added !");
+                id[0] = (int) dataSnapshot.getChildrenCount();
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+                Log.e("101", "Child CHanged !");
+                id[0] = (int) dataSnapshot.getChildrenCount();
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
 
             }
         });
 
+
     }
 
-    private class GeoMessage {
 
+    private static class GeoMessage {
+
+        int id;
         String content;
 
-        public GeoMessage(String content) {
+        public GeoMessage() {};
 
+        public GeoMessage(int id, String content) {
+
+            this.id = id;
             this.content = content;
 
+        }
+
+        public String getContent() {
+            return content;
+        }
+
+        public void setContent(String content) {
+            this.content = content;
         }
     }
 
